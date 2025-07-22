489bb57e6bbe1ba4db4a8feb63fbd0c7e866158d
Amaragonda Adhithya <adhithyaamaragonda@gmail.com>
2025-07-22 20:00:16 +0530

    fix: handle Google-only users in signin

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -21,8 +21,14 @@ export const signin = async (req, res, next) => {
     const validUser = await User.findOne({ email })
     if (!validUser) return next(errorHandler(404, 'User not found!'))
 
+    // Check if user has a password (i.e., not a Google-only account)
+    if (!validUser.password) {
+      return next(errorHandler(400, 'Please sign in with Google'))
+    }
+
     const validPassword = bcryptjs.compareSync(password, validUser.password)
     if (!validPassword) return next(errorHandler(401, 'Wrong credentials'))
+
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
     const { password: pass, ...rest } = validUser._doc
 
