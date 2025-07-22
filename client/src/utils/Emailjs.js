bb28209656244912f79dbdc24d4898dec9025e6d
Amaragonda Adhithya <adhithyaamaragonda@gmail.com>
2025-07-22 20:33:53 +0530

    Small mistake

--- client/src/utils/Emailjs.js
+++ client/src/utils/Emailjs.js
@@ -15,14 +15,12 @@ export const sendEmail = (form) => {
 
 export const sendMessage = (form) => {
   const emailjsUserId = import.meta.env.VITE_APP_EMAILJS;
-  return emailjs
-    .sendForm('service_zjv1bbl', 'template_jquqkon', form, emailjsUserId)
-    .then((response) => {
-      return response;
-    })
-    .catch((error) => {
-      throw error;
-    });
+  return emailjs.sendForm(
+    'service_zjv1bbl', // Your EmailJS service ID
+    'template_jquqkon', // Your EmailJS template ID
+    form,
+    emailjsUserId // Your EmailJS public key
+  );
 };
 
 export default { sendEmail, sendMessage };
