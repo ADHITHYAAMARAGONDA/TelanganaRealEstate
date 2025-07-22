90a25cfa046bbc58e83ed2637c353b3304aff7eb
Amaragonda Adhithya <adhithyaamaragonda@gmail.com>
2025-07-22 19:43:50 +0530

    Added port

--- server/index.js
+++ server/index.js
@@ -59,3 +59,8 @@ app.use((err, req, res, next) => {
     message,
   })
 })
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
